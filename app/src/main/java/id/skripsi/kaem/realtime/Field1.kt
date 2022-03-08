1c8c2f8a4f6e9db3565576964e90899147ce59be
Karyadi Mochtar <karyadi_cm13@apps.ipb.ac.id>
2022-03-08 21:00:03 +0700

    firts

---
+++ app/src/main/java/id/skripsi/kaem/realtime/Field1.kt
@@ -0,0 +1,11 @@
+package id.skripsi.kaem.realtime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Field1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_field1)
+    }
+}
---
+++ app/src/main/java/id/skripsi/kaem/realtime/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.skripsi.kaem.realtime
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
