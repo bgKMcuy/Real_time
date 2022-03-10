98b1ec5cf1b3e011d0c049861a13109962bce1ea
Karyadi Mochtar <karyadi_cm13@apps.ipb.ac.id>
2022-03-10 20:30:57 +0700

    push pertama

--- app/src/main/java/id/skripsi/kaem/realtime/Field3.kt
+++ app/src/main/java/id/skripsi/kaem/realtime/Field3.kt
@@ -3,9 +3,9 @@ package id.skripsi.kaem.realtime
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class Field1 : AppCompatActivity() {
+class Field3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_field1)
+        setContentView(R.layout.activity_field3)
     }
 }
--- app/src/main/java/id/skripsi/kaem/realtime/MainActivity.kt
+++ app/src/main/java/id/skripsi/kaem/realtime/MainActivity.kt
@@ -1,11 +1,41 @@
 package id.skripsi.kaem.realtime
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fieldSatu()
+        fieldDua()
+        fieldTiga()
+    }
+
+    private fun fieldSatu(){
+        val buttonField1 = findViewById<Button>(R.id.tombol_1)
+        buttonField1.setOnClickListener {
+            startActivity(Intent(this,Field1::class.java))
+            Toast.makeText(this,"menampilkan field 1", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun fieldDua(){
+        val buttonField2 = findViewById<Button>(R.id.tombol_2)
+        buttonField2.setOnClickListener {
+            startActivity(Intent(this,Field2::class.java))
+            Toast.makeText(this,"menampilkan field 2", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun fieldTiga(){
+        val buttonField3 = findViewById<Button>(R.id.tombol_3)
+        buttonField3.setOnClickListener {
+            startActivity(Intent(this,Field3::class.java))
+            Toast.makeText(this,"menampilkan field 3", Toast.LENGTH_SHORT).show()
+        }
     }
 }
